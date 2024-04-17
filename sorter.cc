42106646446fbf4e0810c971addd709b64ec52e6
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-04-17 12:10:26 +0500

    Определение класса сортировки пользователей: «sorter»

---
+++ sorter.cc
@@ -0,0 +1,62 @@
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+#include "sorter.hh"
+
+sorter::sorter( const std::vector< person >& persons )
+  : _persons{ persons }
+{
+
+}
+
+std::vector<person>
+sorter::operator()( key_sort k )
+{
+  std::vector
+    answer{ _persons };
+
+  std::sort( answer.begin(  ),
+             answer.end(  ),
+             comparator( k )   );
+
+  return answer;
+}
+
+std::function<bool(person, person)>
+sorter::comparator( key_sort key )
+{
+  switch( key )
+  {
+    case key_sort::NAME:
+      return
+        []( const person& left,
+            const person& right )
+        {
+          return left.name() < right.name();
+        };
+
+    case key_sort::SURNAME:
+      return
+        []( const person& left,
+            const person& right )
+        {
+          return left.surname() < right.surname();
+        };
+
+    case key_sort::NUMBER_PHONE:
+      return
+        []( const person& left,
+            const person& right )
+        {
+          return left.number_phone() < right.number_phone();
+        };
+  }
+
+  return
+    []( const person& left,
+        const person& right )
+    {
+      return false;
+    };
+}
---
+++ sorter.hh
@@ -0,0 +1,29 @@
+#ifndef SORTER_HH
+#define SORTER_HH
+
+#include <vector>
+#include <functional>
+
+#include "person.hh"
+#include "key_sort.hh"
+
+class sorter
+{
+private:
+
+  std::vector< person > _persons{};
+
+  std::function< bool( person, person ) >
+  comparator( key_sort key );
+
+public:
+
+  sorter( void ) = delete;
+
+  sorter( const std::vector< person >& persons );
+
+  std::vector<person>
+  operator()( key_sort k );
+};
+
+#endif
