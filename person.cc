ce377649e9dee9eefc1c74d411ca1e0127e016ad
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-04-17 09:35:07 +0500

    Определение класса «person»: имя, фамилия, номер телефона пользователя

---
+++ person.cc
@@ -0,0 +1,51 @@
+#include <string_view>
+#include <utility>
+
+#include "person.hh"
+
+person::person( std::string_view name,
+                std::string_view surname,
+                std::string_view number_phone )
+
+  : _name{ name },
+    _surname{ surname },
+    _number_phone{ number_phone }
+{
+
+}
+
+person::person( const person& p )
+
+  : _name{ p.name(  ) },
+    _surname{ p.surname(  ) },
+    _number_phone{ p.number_phone(  ) }
+{
+
+}
+
+person::person( person&& p )
+
+  : _name{ std::move( p._name ) },
+    _surname{ std::move( p._surname ) },
+    _number_phone{ std::move( p._surname) }
+{
+
+}
+
+std::string
+person::name( void ) const
+{
+  return _name;
+}
+
+std::string
+person::surname( void ) const
+{
+  return _surname;
+}
+
+std::string
+person::number_phone( void ) const
+{
+  return _number_phone;
+}
---
+++ person.hh
@@ -0,0 +1,38 @@
+#ifndef PERSON_HH
+#define PERSON_HH
+
+#include <string>
+#include <string_view>
+
+class person
+{
+private:
+
+  std::string
+    _name{},
+    _surname{},
+    _number_phone{};
+
+public:
+
+  person( void ) = delete;
+
+  person( std::string_view name,
+          std::string_view surname,
+          std::string_view number_phone );
+
+  person( const person& p );
+
+  person( person&& p );
+
+  std::string
+  name( void ) const;
+
+  std::string
+  surname( void ) const;
+
+  std::string
+  number_phone( void ) const;
+};
+
+#endif
